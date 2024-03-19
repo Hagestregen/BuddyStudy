b9fd95fa4281e94e60ad126340641b712dbfd333
Hagestregen <sivert.hb@outlook.com>
2024-03-19 13:36:53 +0100

    Static Files

--- base/forms.py
+++ base/forms.py
@@ -5,3 +5,4 @@ class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
+        exclude = ['host', 'participants'] #This will make sure the host and participants are not sent when creating a new room 'form'. We now have to change the view method to save the user:'host' to the room form
